425788822dfd12c5a1c7f49a4ae15491f6f50827
David Lin <lindavid1998@gmail.com>
2024-12-07 20:03:51 -0800

    Add icon button for changing theme

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
 	const [username, setUsername] = useState('');
 	const [input, setInput] = useState('');
 	const [showResult, setShowResult] = useState(false);
-	// const [theme, setTheme] = useState('dark')
 
 	const handleChange = (e) => {
 		setInput(e.target.value);
@@ -22,25 +21,11 @@ function App() {
 		setShowResult(true);
 	};
 
-	useEffect(() => {
-		document.documentElement.setAttribute('data-theme', 'dark');
-	}, []);
-
-	const toggleTheme = () => {
-		const current = document.documentElement.getAttribute('data-theme');
-		if (current == 'dark') {
-			document.documentElement.setAttribute('data-theme', 'light');
-		} else {
-			document.documentElement.setAttribute('data-theme', 'dark');
-		}
-	};
-
 	return (
 		<div className='content'>
 			<Navbar />
 			<Form onChange={handleChange} onClick={handleClick} />
 			{showResult && <Card username={username} />}
-			<Button onClick={toggleTheme}>Toggle theme</Button>
 		</div>
 	);
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const [theme, setTheme] = useState('dark')
+  
+  const toggleTheme = () => {
+		setTheme((prevTheme) => {
+			const newTheme = prevTheme === 'light' ? 'dark' : 'light';
+			document.documentElement.setAttribute('data-theme', newTheme);
+			return newTheme;
+		});
+	};
+
   return (
-    <div className='navbar'>GitHub Profiles</div>
-  )
+		<div className='navbar'>
+			GitHub Profiles
+			<div className='appearance'>
+				<FontAwesomeIcon icon={theme == 'dark' ? faSun : faMoon} onClick={toggleTheme}/>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar
