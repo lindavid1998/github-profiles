ff15d8b939ddee200cf06b2ce5839373a3689740
David Lin <lindavid1998@gmail.com>
2024-12-07 19:31:06 -0800

    Add toggle between dark and light theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
-import React, { useState, Suspense } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import Card from './components/Card.jsx';
 import Form from './components/Form.jsx';
 import Navbar from './components/Navbar.jsx';
-
+import Button from './components/Button.jsx';
 // TODO: fix bug where previous card shows after closing error message
 // TODO: add form validation (username cant be empty)
 
@@ -11,6 +11,7 @@ function App() {
 	const [username, setUsername] = useState('');
 	const [input, setInput] = useState('');
 	const [showResult, setShowResult] = useState(false);
+	// const [theme, setTheme] = useState('dark')
 
 	const handleChange = (e) => {
 		setInput(e.target.value);
@@ -21,11 +22,25 @@ function App() {
 		setShowResult(true);
 	};
 
+	useEffect(() => {
+		document.documentElement.setAttribute('data-theme', 'dark');
+	}, []);
+
+	const toggleTheme = () => {
+		const current = document.documentElement.getAttribute('data-theme');
+		if (current == 'dark') {
+			document.documentElement.setAttribute('data-theme', 'light');
+		} else {
+			document.documentElement.setAttribute('data-theme', 'dark');
+		}
+	};
+
 	return (
 		<div className='content'>
 			<Navbar />
 			<Form onChange={handleChange} onClick={handleClick} />
 			{showResult && <Card username={username} />}
+			<Button onClick={toggleTheme}>Toggle theme</Button>
 		</div>
 	);
 }
